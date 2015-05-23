c60ee76408c708c10bcc013c86ce0486a50f2994
Kyle Milner <ky@kymbp.local>
2015-05-23 13:08:40 +0100

    Initial Commit

---
+++ Numerals/Numerals.h
@@ -0,0 +1,19 @@
+//
+//  Numerals.h
+//  Numerals
+//
+//  Created by Kyle Milner on 23/05/2015.
+//  Copyright (c) 2015 Kyle Milner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Numerals.
+FOUNDATION_EXPORT double NumeralsVersionNumber;
+
+//! Project version string for Numerals.
+FOUNDATION_EXPORT const unsigned char NumeralsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Numerals/PublicHeader.h>
+
+
